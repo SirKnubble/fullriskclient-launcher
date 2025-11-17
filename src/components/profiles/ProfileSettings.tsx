53354aa6d3f92ba4d2045e64cb97fe56b5d27891
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2025-11-17 07:56:01 +0100

    fix: [LAUNCHER-BUG]: Creating a symlink with a custom location throws an error #2285 https://github.com/NoRiskClient/issues/issues/2285

--- src/components/profiles/ProfileSettings.tsx
+++ src/components/profiles/ProfileSettings.tsx
@@ -386,10 +386,11 @@ export function ProfileSettings({ profile, onClose }: ProfileSettingsProps) {
           <div className="border-l border-white/10 mx-4 my-3 h-[85%]"></div>
         </div>
 
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <div
-            className="flex-1 py-2 pl-0 pr-4 overflow-y-auto custom-scrollbar"
+            className="flex-1 py-2 pl-0 pr-4 overflow-y-auto overflow-x-hidden custom-scrollbar min-w-0"
             ref={contentRef}
+            style={{ maxWidth: '100%', boxSizing: 'border-box' }}
           >
             {renderTabContent()}
           </div>
